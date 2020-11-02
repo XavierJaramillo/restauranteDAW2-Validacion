5e53e1ac769bf25b853847f2cb6fb188213cf9b5
Xavier Jaramillo <xavijvives@gmail.com>
2020-11-02 16:45:11 +0100

    Implementacion impresion mesas con filtro

--- view/zonaRestaurante.php
+++ view/zonaRestaurante.php
@@ -2,7 +2,15 @@
 //MartinezFalconi
 
 //JaramilloVives
+include_once '../model/mesaDAO.php';
 
+echo "<a href='../view/zonaRestaurante.php?espacio=VIPs'>HOLA</a>";
+echo "<a href='../view/zonaRestaurante.php?espacio=Terraza'>HOLA</a>";
+echo "<a href='../view/zonaRestaurante.php?espacio=Comedor'>HOLA</a>";
+$mesaDAO = new MesaDAO();
+echo "<table id='table'>";
+echo $mesaDAO->getMesas();
+echo "</table>"
 //FernandezVives
 
 ?>
