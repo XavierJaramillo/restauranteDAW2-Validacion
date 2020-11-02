719919d7a87651a0f90d2226f14c1905ee50cc52
unknown <5871.joan23@fje.edu>
2020-11-02 20:11:14 +0100

    hoja de estilos completada

--- db/config.php
+++ db/config.php
@@ -1,5 +1,5 @@
 <?php
-define ("BD","restaurante_db");
+define ("BD","restaurante_bd");
 define ("SERVIDOR","localhost");
 define ("USER","root");
 define ("PASSWORD","");
--- db/dbRestaurante.sql
+++ db/dbRestaurante.sql
@@ -20,7 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `restaurante_bd`
 --
-
+CREATE DATABASE restaurante_bd;
+USE restaurante_bd;
 -- --------------------------------------------------------
 
 --
