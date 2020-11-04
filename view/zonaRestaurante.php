741e7fa9041b627c82f5751c0118f472d82a97ce
Xavier Jaramillo <xavijvives@gmail.com>
2020-11-04 17:01:38 +0100

    style

--- view/zonaRestaurante.php
+++ view/zonaRestaurante.php
@@ -3,19 +3,24 @@
         <title>Pagina Principal | Restaurante</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="../css/style.css">
+        <link rel="stylesheet" type="text/css" href="../css/zonaRestaurante.css">
     </head>
     <body>
-        <?php
-            require_once '../controller/sessionController.php';
-        ?>
+        <div class="nav"> 
+            <?php
+                require_once '../controller/sessionController.php';
+            ?>
+        </div>
 
-        <ul>
-        <li><a href='../view/zonaRestaurante.php?espacio=VIPs'>VIPs</a></li>
-        <li><a href='../view/zonaRestaurante.php?espacio=Terraza'>Terraza</a></li>
-        <li><a href='../view/zonaRestaurante.php?espacio=Comedor'>Comedor</a></li>
-        <li class="historico"><a href="./regMesaHorarios.php">Historico</a></li>
-        </ul>
+        <div class="subnav">
+            
+            <a href='../view/zonaRestaurante.php?espacio=VIPs'>VIPs</a>
+            <a href='../view/zonaRestaurante.php?espacio=Terraza'>Terraza</a>
+            <a href='../view/zonaRestaurante.php?espacio=Comedor'>Comedor</a>
+            <a href="./regMesaHorarios.php">Historico</a>
+            
+        </div>
+        
             
         <?php
         include_once '../model/mesaDAO.php';
