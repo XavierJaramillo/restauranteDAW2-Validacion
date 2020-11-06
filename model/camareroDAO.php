3408bcd631ad2ed0b54d24b8b5bc186d64a963bd
MartinezFalconi <sergiomafa09@gmail.com>
2020-11-06 19:15:31 +0100

    v.3.0

---
+++ model/camareroDAO.php
@@ -0,0 +1,37 @@
+<?php
+class camareroDAO {
+    // ATRIBUTOS
+    private $pdo;
+
+    // CONSTRUCTOR
+    public function __construct() {
+        include_once '../db/connection.php';
+        $this->pdo=$pdo;
+    }
+
+    // VALIDACIÓN DEL LOGIN
+    // DEVUELVE TRUE EN CASO DE QUE EN LA BASE DE DATOS HAYA UN CAMARERO CON NOMBRE Y CONTRASEÑA IGUALES A LA QUE EL
+    // USUARIO APORTA EN EL FORMULARIO DEL LOGIN. FALSE, EN CUALQUIER OTRO CASO
+    public function login($camarero){
+        $query = "SELECT * FROM `camareros` WHERE `nombre_camarero`=? AND `pass_camarero`=?";
+        $sentencia=$this->pdo->prepare($query);
+        $nombre = $camarero->getNombre_camarero();
+        $pass = $camarero->getPass_camarero();
+        $sentencia->bindParam(1,$nombre);
+        $sentencia->bindParam(2,$pass);
+        $sentencia->execute();
+        $result=$sentencia->fetch(PDO::FETCH_ASSOC);
+        $numRow=$sentencia->rowCount();
+        if(!empty($numRow) && $numRow==1){
+            $camarero->setId_camarero($result['id_camarero']);
+            $camarero->setIdMantenimiento($result['idMantenimiento']);
+            session_start();
+            $_SESSION['camarero']=$camarero;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+?>
---
+++ view/login.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <title>Inicia sesión | Restaurante</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="../css/style.css">
+        <script src="../js/code.js"></script>
+    </head>
+
+    <body>
+    
+    <div class="registro">
+        <img class="logoLogin" src="../img/logo.png" alt="Logo restaurante">
+        <!-- FORMULARIO CON LOS DOS CAMPOS A RELLENAR (NOMBRE Y CONTRASEÑA), ESTE FORMULARIO SE VALIDA EN LOGINCONTROLLER.PHP -->
+        <!-- EL METODO UTILIZADO PARA ENVIAR LOS INPUTS ES POR POST -->
+        <form action="../controller/loginController.php" method="POST" onsubmit="return validacionLogin()">
+            <p>Nombre de usuario: </p>
+            <input type="text" name="user" id="user" >
+            <p>Contraseña: </p>
+            <input type="password" name="pass" id="pass" ><br>
+            <input type="submit" value="Iniciar sesión">
+        </form>
+    </div>
+
+    </body>
+</html>
